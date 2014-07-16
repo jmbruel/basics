26a3d22e05c5135f2a03e87f2dcf04a877d5edc1
Jean-Michel Bruel <jbruel#travis@gmail.com>
2014-07-16 17:22:01 +0200

    update of the README about CI

--- update.sh
+++ update.sh
@@ -2,21 +2,21 @@
 echo -e "Starting to update gitHub\n"
 
 #copy data we're interested in to other place
-cp -R *.png $HOME/output
+#cp -R *.png $HOME/output
 
 #go to home and setup git
 cd $HOME
 git config --global user.email "jbruel#travis@gmail.com"
 git config --global user.name "Jean-Michel Bruel"
-git config --global push.default simple
+#git config --global push.default simple
 
 #using token clone io pages branch
-git clone --quiet https://github.com/jmbruel/basics.git travis > /dev/null
+#git clone --quiet https://github.com/jmbruel/basics.git travis > /dev/null
 
 #go into directory and copy data we're interested in to that directory
-cd travis
-cp $HOME/output/*.png .
-ls -al 
+#cd travis
+#cp $HOME/output/*.png .
+#ls -al 
 
 #add, commit and push files without provoking Continuous Integration
 git add *.png
