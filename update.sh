808ccc151cc4d07a2b6d8535609a2f412838b543
Jean-Michel Bruel <jbruel#travis@gmail.com>
2014-07-18 18:01:58 +0200

    token for travis

--- update.sh
+++ update.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-echo -e "Starting to update gitHub\n"
+echo -e "Starting to update doc\n"
 
 #copy data we're interested in to other place
-#cp -R *.png $HOME/output
+cp -R output $HOME/output
 
 #go to home and setup git
 cd $HOME
 git config --global user.email "jbruel#travis@gmail.com"
 git config --global user.name "Jean-Michel Bruel"
-#git config --global push.default simple
+git config --global push.default simple
 
 #using token clone io pages branch
-#git clone --quiet https://github.com/jmbruel/basics.git travis > /dev/null
+git clone --quiet https://${GH_TOKEN}@github.com/jmbruel/jmbruel.github.io.git  doc > /dev/null
 
 #go into directory and copy data we're interested in to that directory
-#cd travis
-#cp $HOME/output/*.png .
-#ls -al 
+cd doc
+cp $HOME/output/faq.html ./index.html
+ls -al index.html
 
 #add, commit and push files without provoking Continuous Integration
-git add *.png
-git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to basics repository -- [skip ci]"
+git add -f index.html
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to io pages -- [skip ci]"
 git push -fq origin master > /dev/null
 
 echo -e "Done magic with output\n"
